10118bd58bd4fb70e5fe66edb74fd00ccf7ad37b
valeriy <epsm@i.ua>
2016-02-15 23:03:53 +0200

    not in eclipse: run worsks, test works

---
+++ src/main/java/com/epsm/client/GreetingServiceAsync.java
@@ -0,0 +1,7 @@
+package com.epsm.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
