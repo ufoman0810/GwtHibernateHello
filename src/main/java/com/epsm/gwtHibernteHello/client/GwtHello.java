638a3f14f221e0aae275711b457b370a4fc3f352
valeriy <epsm@i.ua>
2016-02-17 22:19:30 +0200

    Logging added.

--- src/main/java/com/epsm/gwtHibernteHello/client/GwtHello.java
+++ src/main/java/com/epsm/gwtHibernteHello/client/GwtHello.java
@@ -1,6 +1,8 @@
 package com.epsm.gwtHibernteHello.client;
 
-import com.epsm.gwtHibernteHello.client.page.Login;
+import java.util.logging.Logger;
+
+import com.epsm.gwtHibernteHello.client.page.LoginPage;
 import com.epsm.gwtHibernteHello.client.service.LoginService;
 import com.epsm.gwtHibernteHello.client.service.LoginServiceAsync;
 import com.google.gwt.core.client.EntryPoint;
@@ -11,24 +13,32 @@ import com.google.gwt.user.client.ui.RootLayoutPanel;
 public class GwtHello implements EntryPoint {
 	private LoginServiceAsync loginService = GWT.create(LoginService.class);
 	private String sessionId;
+	private static Logger logger = Logger.getLogger("GwtHello");
 	
 	@Override
 	public void onModuleLoad() {
+		logger.info("Requested: GwtHello page.");
 		getSessionIdFromCookies();
 		
 		if(sessionId == null && isSeesionStillNotExpiredOnServer()){
 			displayGreetUserPage();
+			logger.fine("Displayed: GreetUserPage.");
 		}else {
 			displayLoginPage();
+			logger.fine("Displayed: LoginPage.");
 		}
 	}
 	
 	private void getSessionIdFromCookies(){
 		sessionId = Cookies.getCookie("sessionId");
+		logger.fine("Invoked: getSessionIdFromCookies() method, sessionId = '" + sessionId + "'.");
 	}
 
 	private boolean isSeesionStillNotExpiredOnServer(){
-		return false;
+		boolean notExpired = false;
+		logger.fine("Invoked: isSeesionStillNotExpiredOnServer() method, returned '" + notExpired + "'.");
+	
+		return notExpired;
 	}
 	
 	private void displayGreetUserPage(){
@@ -36,6 +46,6 @@ public class GwtHello implements EntryPoint {
 	}
 	
 	private void displayLoginPage(){
-		RootLayoutPanel.get().add(new Login());
+		RootLayoutPanel.get().add(new LoginPage());
 	}
 }
--- src/main/java/com/epsm/gwtHibernteHello/client/page/LoginPage.java
+++ src/main/java/com/epsm/gwtHibernteHello/client/page/LoginPage.java
@@ -1,5 +1,7 @@
 package com.epsm.gwtHibernteHello.client.page;
 
+import java.util.logging.Logger;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -12,13 +14,15 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-public class Login extends Composite {
+public class LoginPage extends Composite {
 	private static LoginUiBinder uiBinder = GWT.create(LoginUiBinder.class);
 	private int loginLenght;
 	private int passwordLenght;
+	private final int MIN_LENGHT = 4;
+	private static Logger logger = Logger.getLogger("Login");
 	   
-	@UiTemplate("Login.ui.xml")
-	interface LoginUiBinder extends UiBinder<Widget, Login> {
+	@UiTemplate("LoginPage.ui.xml")
+	interface LoginUiBinder extends UiBinder<Widget, LoginPage> {
 	}
 
 	@UiField(provided = true)
@@ -33,34 +37,49 @@ public class Login extends Composite {
 	@UiField
 	Label errorLabel;
 	
-	public Login() {
+	public LoginPage() {
 		resources = GWT.create(Resources.class);
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
 	@UiHandler("buttonLogin")
 	void doClickLogin(ClickEvent event) {
+		logger.fine("Pressed: logging button.");
+		
 		if (areLoginOrPasswordTooShort()) {
-			errorLabel.setText("Login or password can't be shorter than 4 chars.");
+			String errorMessage = "Login or password can't be shorter than " + MIN_LENGHT + " chars.";
+			errorLabel.setText(errorMessage);
+			logger.fine("Result: login button pressed - error, " + errorMessage);
 		} else {
 			Window.alert("Login Successful!");
+			logger.fine("Result: login button pressed - login successful");
 		}
 	}
 	
 	private boolean areLoginOrPasswordTooShort(){
 		getLoginAndPasswordLenghts();
+		boolean tooShort = areLenghtsShorterThanMin();
+		logger.finest("Invoked: areLoginOrPasswordTooShort() method, returned '" + tooShort + "'.");
 		
-		return compareLenghtsWithMinimalValue();
+		return tooShort;
 	}
 	
 	private void getLoginAndPasswordLenghts(){
 		loginLenght = loginBox.getText().length();
 		passwordLenght = passwordBox.getText().length();
+		logger.finest("Invoked: getLoginAndPasswordLenghts() method, "
+				+ "login lenght = '" + loginLenght + "', "
+				+ "password lenght = '" + passwordLenght + "'.");
 	}
 	
-	private boolean compareLenghtsWithMinimalValue(){
-		final int minValue = 4;
+	private boolean areLenghtsShorterThanMin(){
+		boolean shorter = loginLenght < MIN_LENGHT || passwordLenght < MIN_LENGHT;
+		logger.finest("Invoked: areLenghtsShorterThanMin() method, "
+				+ " min value = '" + MIN_LENGHT + "', "
+				+ "login lenght =  '" + loginLenght + "', "
+				+ "password lenght = '" + passwordLenght + "', "
+				+ "returned '" + shorter + "'.");
 		
-		return loginLenght < minValue || passwordLenght < minValue;
+		return shorter;
 	}
 }
